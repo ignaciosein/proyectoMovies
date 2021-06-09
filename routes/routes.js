5673808447010b894ba4acd62246e869d242820d
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-06-09 17:36:00 +0200

    cambiado el routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,22 @@ const router = require("express").Router();
 const pages = require("../controllers/pages")
 
 router.get("/", pages.home);
-router.post("/", pages.postHome)
+router.post("/", pages.postLogin)
+router.post("/singup", pages.postSingUp)
+ 
+router.get("/dashboard", pages.getDashboard)
+router.get("/search", pages.getSearch)
+router.get("/search/:title", pages.getSearchTitle)
+router.get("/movies", pages.getMovies)
 
+router.post("/signup", pages.postSignup)
+router.post("/login", pages.postLogin)
+router.post("/createMovie", pages.postMakeMovie)
 
+router.put("/editMovie/:id", pages.putMovie)
 
+router.delete("/editMovie/:id", pages.delMovie)
+
+/*   dasdasdasdasd */
 
 module.exports = router;
