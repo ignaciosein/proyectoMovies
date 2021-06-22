e65cbc10b4d4d1cd1ef66525d177973dade80595
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-06-22 20:30:19 +0200

    menu burger creado

---
+++ Public/js/menu.js
@@ -0,0 +1,17 @@
+
+document.getElementById("menuIcon").addEventListener("click", function () {
+  document.getElementById("menuIcon").style.display = "none";
+
+  document.getElementById("containerMenu").style.display = "block";
+
+  document.getElementById("menuIcon2").style.display = "block";
+});
+
+
+document.getElementById("menuIcon2").addEventListener("click", function () {
+    document.getElementById("menuIcon").style.display = "block";
+  
+    document.getElementById("containerMenu").style.display = "none";
+  
+    document.getElementById("menuIcon2").style.display = "none";
+  });
