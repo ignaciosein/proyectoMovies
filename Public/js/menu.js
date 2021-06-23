321c9235b5b90951783bba8d48d209a2e0732149
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-06-23 12:31:11 +0200

    Diseños de dashboard,search,favmovies

--- Public/js/menu.js
+++ Public/js/menu.js
@@ -1,5 +1,7 @@
-
+ 
 document.getElementById("menuIcon").addEventListener("click", function () {
+
+/*   alert("el menu se abre") */
   document.getElementById("menuIcon").style.display = "none";
 
   document.getElementById("containerMenu").style.display = "block";
@@ -9,6 +11,7 @@ document.getElementById("menuIcon").addEventListener("click", function () {
 
 
 document.getElementById("menuIcon2").addEventListener("click", function () {
+/*   alert("el menu se cierra") */
     document.getElementById("menuIcon").style.display = "block";
   
     document.getElementById("containerMenu").style.display = "none";
